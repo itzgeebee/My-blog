568b462383b3c65a98315e75e3e43fc8226e87f8
Gideon <balogungideon12@gmail.com>
2022-03-19 14:07:11 +0100

    requirements

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ gravatar = Gravatar(app,
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.debug = True
+
 
 
 
